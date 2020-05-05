f193c41126d2f9f19d87bef4353468e3036245c0
Diana Garbuzova <lessarea@gmail.com>
2020-05-05 13:22:15 +0300

    modules&services: add auth route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CoursesComponent } from './courses/courses.component';
+import { LoginPageComponent } from './auth/login-page/login-page.component';
 
 const routes: Routes = [
   { path: 'courses', component: CoursesComponent },
+  { path: 'auth', component: LoginPageComponent },
   { path: '', redirectTo: '/courses', pathMatch: 'full' },
   { path: '**', component: CoursesComponent },
 ];
